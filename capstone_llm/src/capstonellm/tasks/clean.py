8dc3348abc382303cc2895f53ea27c7b11fd6882
Thomas Neilssen <thomasnelissen@gmail.com>
2024-09-12 13:08:18 +0000

    first code

--- capstone_llm/src/capstonellm/tasks/clean.py
+++ capstone_llm/src/capstonellm/tasks/clean.py
@@ -1,5 +1,7 @@
 import argparse
 import logging
+import subprocess
+import os
 from pyspark.sql import SparkSession
 
 from capstonellm.common.spark import ClosableSparkSession
@@ -7,7 +9,27 @@ from capstonellm.common.spark import ClosableSparkSession
 logger = logging.getLogger(__name__)
 
 def clean(spark: SparkSession, environment: str, tag: str):
-    pass
+    # download the ingested data from S3 for the given tag
+    print(f"Downloading {tag}/question.json")
+    if environment == "local" and not os.path.isfile(f"./data_in/{tag}/questions.json"):
+        subprocess.run(["aws", "s3", "cp", f"s3://dataminded-academy-capstone-llm-data-us/input/{tag}/questions.json", f"./data_in/{tag}/"])
+    print(f"Downloading {tag}/answers.json")
+    if environment == "local" and not os.path.isfile("./data_in/" +tag +"/answers.json"):
+        subprocess.run(["aws", "s3", "cp", f"s3://dataminded-academy-capstone-llm-data-us/input/{tag}/answers.json", f"./data_in/{tag}/"])
+
+    # process the data
+    questions = spark.read.json(f"./data_in/{tag}/questions.json")
+    answers = spark.read.json(f"./data_in/{tag}/answers.json")
+
+    questions_w_answers = (
+        questions
+            .join(answers, on="question_id", how="inner")
+            .select(questions.title, questions.body.alias("question"), answers.body.alias("answer"))
+    )
+
+    questions_w_answers.to_json(f"./data_out/{tag}.json")
+
+    
 
 def main():
     parser = argparse.ArgumentParser(description="capstone_llm")
