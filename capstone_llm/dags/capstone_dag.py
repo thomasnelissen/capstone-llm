2f0da6397e51cc94c8e9f8e6e411cd6feedda8e5
Thomas Neilssen <thomasnelissen@gmail.com>
2024-09-13 09:54:21 +0000

    updated the conveyor DAG to use a ConveyorContainerOperator

--- capstone_llm/dags/capstone_dag.py
+++ capstone_llm/dags/capstone_dag.py
@@ -1,9 +1,8 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.providers.docker.operators.docker import DockerOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.utils.trigger_rule import TriggerRule
-import os
+from conveyor.operators import ConveyorContainerOperatorV2
 
 default_args = {
     "owner": "airflow",
@@ -22,42 +21,36 @@ with DAG(
     schedule_interval="0 0 * * *",
     catchup=False,
 ) as dag:
+    role = "arn:aws:iam::338791806049:role/seasonal-school-gitpod-role"
     dag.doc_md = __doc__
-    # ingest_task = DockerOperator(
-    #     task_id="capstone_ingest",
+    tags = ["airflow", "apache-spark", "dbt", "docker", "pyspark", "python-polars", "sql"]
+    tasks = [
+        ConveyorContainerOperatorV2(
+            task_id="capstone_clean_" + tag,
+            trigger_rule=TriggerRule.ALL_DONE,
+            image="capstone",
+            cmds=["python3", "-m", "capstonellm.tasks.clean", "-e production", "-t " + tag],
+            aws_role=role,
+            instance_type='mx.micro',
+        ) for tag in tags
+    ]
+    # tasks = [
+    #     DockerOperator(
+    #     task_id="capstone_clean_" + tag,
+    #     trigger_rule=TriggerRule.ALL_DONE,
     #     image="capstone",
-    #     container_name="capstone_ingest",
+    #     container_name="capstone_clean_" + tag,
     #     api_version="auto",
     #     auto_remove=True,
-    #     command="python3 -m capstonellm.tasks.ingest -t pyspark",
+    #     command="python3 -m capstonellm.tasks.clean -e production -t " + tag,
     #     environment={
-    #         "AWS_ACCESS_KEY_ID": "{{ env['AWS_ACCESS_KEY_ID'] }}",
-    #         "AWS_SECRET_ACCESS_KEY": "{{ env['AWS_SECRET_ACCESS_KEY']' }}",
-    #         "AWS_SESSION_TOKEN": "{{ env['AWS_SESSION_TOKEN'] }}"
+    #         "AWS_ACCESS_KEY_ID": os.getenv("AWS_ACCESS_KEY_ID"),
+    #         "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_SECRET_ACCESS_KEY"),
+    #         "AWS_SESSION_TOKEN": os.getenv("AWS_SESSION_TOKEN")
     #     },
     #     docker_url="unix://var/run/docker.sock",
-    #     network_mode="bridge",
-    # )
-
-    tags = ["airflow", "apache-spark", "dbt", "docker", "pyspark", "python-polars", "sql"]
-
-    tasks = [
-        DockerOperator(
-        task_id="capstone_clean_" + tag,
-        trigger_rule=TriggerRule.ALL_DONE,
-        image="capstone",
-        container_name="capstone_clean_" + tag,
-        api_version="auto",
-        auto_remove=True,
-        command="python3 -m capstonellm.tasks.clean -e production -t " + tag,
-        environment={
-            "AWS_ACCESS_KEY_ID": os.getenv("AWS_ACCESS_KEY_ID"),
-            "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_SECRET_ACCESS_KEY"),
-            "AWS_SESSION_TOKEN": os.getenv("AWS_SESSION_TOKEN")
-        },
-        docker_url="unix://var/run/docker.sock",
-        network_mode="bridge") for tag in tags
-    ]
+    #     network_mode="bridge") for tag in tags
+    # ]
 
     empty = EmptyOperator(
         task_id="dummy"
